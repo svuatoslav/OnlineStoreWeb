121b5f4e2353e4674d0237bbcbe79f3e5f73cf8f
svuatoslav <moskalenko.sv.s@mail.ru>
2024-11-02 23:31:49 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineStoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
